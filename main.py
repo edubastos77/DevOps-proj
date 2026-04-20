c1e5a43d5aeab59df3351658fb782e317dd0bb27
edubastos77 <edubastosmail@gmail.com>
2026-04-20 15:26:31 -0300

    modificacao endpoints
    nova endpoints

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # http://127.0.0.1:8000
-@app.get("/")
+@app.get("/hello")
 async def root():
     return {"message": "Hello Word!"}
 
 # http://127.0.0.1:8000/test1
-@app.get("/test1")
+@app.get("/funcao")
 async def test():
     return {"teste": "deu certo."}
