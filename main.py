512596d832a5e89aa401f2c7e1b8e9ab67636392
edubastos77 <edubastosmail@gmail.com>
2026-04-20 15:31:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # http://127.0.0.1:8000
-@app.get("/hello")
+@app.get("/")
 async def root():
     return {"message": "Hello Word!"}
 
 # http://127.0.0.1:8000/test1
+<<<<<<< Updated upstream
 @app.get("/funcao")
+=======
+@app.get("/funcao")
+>>>>>>> Stashed changes
 async def test():
     return {"teste": "deu certo."}
